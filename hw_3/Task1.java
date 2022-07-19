c17b315205fb4429184b45ab6fd592de0e2d2d33
ShcharbitskiAV <107205315+ShcharbitskiAV@users.noreply.github.com>
2022-07-19 23:08:08 +0300

    Update Task1.java

--- hw_3/Task1.java
+++ hw_3/Task1.java
@@ -1,12 +1,11 @@
 public class Task1 {
     public static void main(String[] args) {
-        for(int i = 1; i <= 6; i++){
-            for(int j = 0; i > 6; j--)
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; i <= j; j--)
                 System.out.print(" ");
-            for(int k = 1; k <= i; k++)
+            for (int k = 0; k < i; k++)
                 System.out.print("*");
             System.out.println();
         }
     }
 }
-
